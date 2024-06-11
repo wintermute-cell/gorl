d204407f4ecd0a7444ff1811b2bf1153c40b701e
wintermute <kjulius@gmx.de>
2024-06-11 14:27:07 +0200

    add constructor for Maybe type

--- fw/core/datastructures/maybe.go
+++ fw/core/datastructures/maybe.go
@@ -7,6 +7,16 @@ type Maybe[T any] struct {
 	has   bool
 }
 
+// NewMaybe creates a new Maybe container with the given value.
+func NewMaybe[T any](value T) Maybe[T] {
+	return Maybe[T]{value, true}
+}
+
+// EmptyMaybe creates a new Maybe container without a value.
+func EmptyMaybe[T any]() Maybe[T] {
+	return Maybe[T]{has: false}
+}
+
 // Get returns the value and a bool indicating if the value is ok (true) or considered
 // "empty" (false).
 func (m *Maybe[T]) Get() (T, bool) {
