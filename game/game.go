73db7f551e04978e6db52cdf4691a9755674cc50
wintermute <kjulius@gmx.de>
2024-06-11 17:01:19 +0200

    clean trash after merge

--- game/game.go
+++ game/game.go
@@ -1,19 +1,6 @@
 package game
 
-import (
-	"gorl/fw/core/store"
-	"gorl/fw/modules/scenes"
-	gscenes "gorl/game/scenes"
-)
-
-type ControlState struct {
-	SliderVal float32
-}
+import ()
 
 func Init() {
-	cs := ControlState{}
-	store.Add(cs)
-
-	scenes.RegisterScene("VFH", &gscenes.VfhScene{})
-	scenes.EnableScene("VFH")
 }
