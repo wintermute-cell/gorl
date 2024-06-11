c804f70d724811b7609795686aba74d20fadd4c2
wintermute <kjulius@gmx.de>
2024-06-11 14:20:19 +0200

    add GetVertices for colliders

--- fw/physics/debug.go
+++ fw/physics/debug.go
@@ -5,6 +5,8 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+// Deprecated: does not work with new cameras / rendering.
+// Instead use myCollder.GetVectices() and draw them yourself.
 func DrawColliders(draw_shapes, draw_joints, draw_bounding bool) {
 	if draw_shapes { // shape
 		for b := State.physicsWorld.GetBodyList(); b != nil; b = b.GetNext() {
@@ -32,7 +34,7 @@ func DrawColliders(draw_shapes, draw_joints, draw_bounding bool) {
 					proxy := f.M_proxies[i]
 					aabb := bp.GetFatAABB(proxy.ProxyId)
 					vs := [4]rl.Vector2{}
-                    vs[0] = simulationToPixelScaleV(rl.Vector2{X: float32(aabb.LowerBound.X), Y: float32(aabb.LowerBound.Y)})
+					vs[0] = simulationToPixelScaleV(rl.Vector2{X: float32(aabb.LowerBound.X), Y: float32(aabb.LowerBound.Y)})
 					vs[1] = simulationToPixelScaleV(rl.Vector2{X: float32(aabb.UpperBound.X), Y: float32(aabb.LowerBound.Y)})
 					vs[2] = simulationToPixelScaleV(rl.Vector2{X: float32(aabb.UpperBound.X), Y: float32(aabb.UpperBound.Y)})
 					vs[3] = simulationToPixelScaleV(rl.Vector2{X: float32(aabb.LowerBound.X), Y: float32(aabb.UpperBound.Y)})
@@ -44,42 +46,42 @@ func DrawColliders(draw_shapes, draw_joints, draw_bounding bool) {
 }
 
 func drawShape(fixture *box2d.B2Fixture, transform box2d.B2Transform) {
-    if fixture.GetType() == box2d.B2Shape_Type.E_circle {
-        pos := rl.NewVector2(float32(transform.P.X), float32(transform.P.Y))
-        pos = simulationToPixelScaleV(pos)
-        radius := fixture.GetShape().GetRadius()
-        radius = simulationToPixelScale(radius)
-        rl.DrawCircleV(pos, float32(radius), rl.Red)
-    } else if fixture.GetType() == box2d.B2Shape_Type.E_polygon {
-        polygonShape := fixture.GetShape().(*box2d.B2PolygonShape) // Cast to specific shape type
-        vertexCount := polygonShape.M_count
+	if fixture.GetType() == box2d.B2Shape_Type.E_circle {
+		pos := rl.NewVector2(float32(transform.P.X), float32(transform.P.Y))
+		pos = simulationToPixelScaleV(pos)
+		radius := fixture.GetShape().GetRadius()
+		radius = simulationToPixelScale(radius)
+		rl.DrawCircleV(pos, float32(radius), rl.Red)
+	} else if fixture.GetType() == box2d.B2Shape_Type.E_polygon {
+		polygonShape := fixture.GetShape().(*box2d.B2PolygonShape) // Cast to specific shape type
+		vertexCount := polygonShape.M_count
 
-        // Prepare a slice to hold our transformed vertices
-        vs := make([]rl.Vector2, vertexCount)
+		// Prepare a slice to hold our transformed vertices
+		vs := make([]rl.Vector2, vertexCount)
 
-        for i := 0; i < vertexCount; i++ {
-            // Transform the vertex
-            // TODO: what is even going on here
-            transformedVertex := polygonShape.M_vertices[i] //box2d.B2TransformVec2Mul(transform, polygonShape.M_vertices[i])
+		for i := 0; i < vertexCount; i++ {
+			// Transform the vertex
+			// TODO: what is even going on here
+			transformedVertex := polygonShape.M_vertices[i] //box2d.B2TransformVec2Mul(transform, polygonShape.M_vertices[i])
 
-            // Convert to raylib Vector2 and scale
-            vpos := rl.NewVector2(float32(transformedVertex.X), float32(transformedVertex.Y))
-            pos := rl.NewVector2(float32(transform.P.X), float32(transform.P.Y))
-            vs[i] = simulationToPixelScaleV(rl.Vector2Add(pos, vpos))
-        }
+			// Convert to raylib Vector2 and scale
+			vpos := rl.NewVector2(float32(transformedVertex.X), float32(transformedVertex.Y))
+			pos := rl.NewVector2(float32(transform.P.X), float32(transform.P.Y))
+			vs[i] = simulationToPixelScaleV(rl.Vector2Add(pos, vpos))
+		}
 
-        // Draw the polygon using raylib
-        drawPolygon(vs)
-    }
+		// Draw the polygon using raylib
+		drawPolygon(vs)
+	}
 }
 
 // Draw a polygon in pixel coordinates
 func drawPolygon(points []rl.Vector2) {
-    for i, point := range points {
-        if i < len(points) - 1 {
-            rl.DrawLineV(point, points[i+1], rl.Color{R: 255, G: 0, B: 0, A: 100})
-        } else {
-            rl.DrawLineV(point, points[0], rl.Color{R: 255, G: 0, B: 0, A: 100})
-        }
-    }
+	for i, point := range points {
+		if i < len(points)-1 {
+			rl.DrawLineV(point, points[i+1], rl.Color{R: 255, G: 0, B: 0, A: 100})
+		} else {
+			rl.DrawLineV(point, points[0], rl.Color{R: 255, G: 0, B: 0, A: 100})
+		}
+	}
 }
